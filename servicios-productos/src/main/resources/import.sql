b0439a291c1c68cdc2e8be76cb0919c3baf750cc
Developsteven <steven.perez099@gmail.com>
2021-08-16 17:50:08 -0500

    Productos

---
+++ servicios-productos/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into productos (nombre, precio, create_at) values ('Panasonic', 820, now());
+insert into productos (nombre, precio, create_at) values ('Sony', 700, now());
+insert into productos (nombre, precio, create_at) values ('Apple', 1000, now());
+insert into productos (nombre, precio, create_at) values ('Xiaomi', 600, now());
+insert into productos (nombre, precio, create_at) values ('Logitech', 300, now());
+insert into productos (nombre, precio, create_at) values ('Lenovo', 400, now());
+insert into productos (nombre, precio, create_at) values ('Samsum', 350, now());
